d06762e270427aa17ab93c61005ca9a91e488bfb
rfalgo <robaldgom@gmail.com>
2023-02-25 15:07:59 -0500

    The file is updated

--- hola.py
+++ hola.py
@@ -1,3 +1,8 @@
+import argparse
 def say_hello(name="World"):
     print("Hello ", name, "!")
-say_hello()
+parser = argparse.ArgumentParser(description='Say hello.')
+parser.add_argument('--name', type=str, required=False,
+metavar='name', help='your name', default='World')
+args = parser.parse_args()
+say_hello(args.name)
